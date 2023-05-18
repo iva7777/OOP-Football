d92b43e57960f0cb3bb93a0a9e034c4ced58efde
Iva <60818270+iva7777@users.noreply.github.com>
2023-05-18 16:16:22 +0300

    Create Team.cs
    create Game.cs and the functionality

---
+++ ModelMeThisFootball/Team.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelMeThisFootball
+{
+    public class Team
+    {
+        public Coach Coach { get; set; }
+        private List<FootballPlayer> players;
+
+        public List<FootballPlayer> Players
+        {
+            get { return players; }
+            set
+            {
+                if (value.Count >= 11 && value.Count <= 22)
+                {
+                    players = value;
+                }
+                else
+                    throw new ArgumentException("The team should have between 11 and 22 players.");
+            }
+        }
+
+        public double PlayersAverageAge
+        {
+            get
+            {
+                if (Players.Count == 0)
+                {
+                    return 0;
+                }
+
+                int totalAge = 0;
+                foreach (FootballPlayer player in Players)
+                {
+                    totalAge += player.Age;
+                }
+
+                return (double)totalAge / Players.Count;
+            }
+        }
+    }
+}
