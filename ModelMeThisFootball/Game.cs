fd46d9609f1b9b2bcb7a23ad893939a735505c3c
Iva <60818270+iva7777@users.noreply.github.com>
2023-05-18 21:25:34 +0300

    Create Game.cs
    find a way to display the goals: minute + player; also implement logic for finishing the game

---
+++ ModelMeThisFootball/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelMeThisFootball
+{
+    public class Game
+    {
+        public Team Team1 { get; set; }
+        public Team Team2 { get; set; }
+        public Referee Referee { get; set; }
+        public Referee AssistantReferee1 { get; set; }
+        public Referee AssistantReferee2 { get; set; }
+        public List<Tuple<int, string>> Goals { get; set; }
+        public string GameResult { get; set; }
+        public Team Winner { get; set; }
+
+        public Game()
+        {
+            Team1 = new Team();
+            Team2 = new Team();
+            Goals = new List<Tuple<int, string>>();
+        }
+
+        public void AddGoal(int minute, string player)
+        {
+            Goals.Add(new Tuple<int, string>(minute, player));
+        }
+
+        public void EndGame()
+        {
+
+        }
+
+    }
+}
